ef77935fa54c92e6cecbdf17da1123b9e5e84261
Michael Perez <maicolpr19@gmail.com>
2024-12-09 21:12:57 -0500

    Cambios nuevos

--- src/main/java/com/educonecta/app/dao/IUsuarioDao.java
+++ src/main/java/com/educonecta/app/dao/IUsuarioDao.java
@@ -2,13 +2,14 @@ package com.educonecta.app.dao;
 
 import java.util.List;
 
-
+import com.educonecta.app.dto.UsuarioDetallesDTO;
 import com.educonecta.app.entity.Usuario;
 
 public interface IUsuarioDao {
 	List<Usuario> ListarUsuarios();
 	Usuario buscarPorId(String id);
 	Usuario buscarPorCorreo(String correo);
+	UsuarioDetallesDTO detallesUsuario(String usuarioId);
 	boolean registrarUsuario(Usuario usuario);
 	boolean borrarUsuario(String id);
 	boolean actualizarContrasena(String UsuarioId, String ContrasenaNew, String ContrasenaOld);
